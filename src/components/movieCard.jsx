6fc6e043cfe9f19abf37767cbe3421ff0fafa3c2
fazm1 <farid.azzam.2001@gmail.com>
2025-01-05 09:59:35 +0200

    key adjustment

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -1,14 +1,14 @@
 import { useNavigate } from "react-router";
 
 function MovieCard(props) {
-    const { movieItem, key } = props;
+    const { movieItem } = props;
     const navigate = useNavigate();
     const handleClick = (id) =>{
         navigate(`/moviedetails/${id}`);
     }
   return (
     <>
-      <div onClick={()=>handleClick(movieItem.id)} className="movieCard" key={key}>
+      <div onClick={()=>handleClick(movieItem.id)} className="movieCard" key={movieItem.id}>
         <img src={"https://image.tmdb.org/t/p/w1280"+movieItem.poster_path} className="movieImage" />
         <div className="cardContent">
             <span className="movieTitle">{movieItem.original_title}</span> <br />
